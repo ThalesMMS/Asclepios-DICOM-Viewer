e91d61a509b75bcf5f15f065e45d8932c86ffa4e
Thales Matheus <133025183+ThalesMMS@users.noreply.github.com>
2025-10-16 08:19:23 -0700

    Handle 2D render failures

--- src/gui/widget2d.h
+++ src/gui/widget2d.h
@@ -2,6 +2,9 @@
 
 #include <qfuture.h>
 #include <qscrollbar.h>
+#include <QLabel>
+#include <QPointer>
+#include <QString>
 #include <QVTKOpenGLNativeWidget.h>
 #include <vtkEventQtSlotConnect.h>
 #include "ui_widget2d.h"
@@ -28,35 +31,38 @@ namespace asclepios::gui
 		
 		void render() override;
 		
-	signals:
-		void imageReaderInitialized();
-		
-	public slots:
-		void onActivateWidget(const bool& t_flag);
-		void onApplyTransformation(const transformationType& t_type) const;
-		void onRefreshScrollValues(core::Series* t_series, core::Image* t_image);
-		void onChangeScrollValue(vtkObject* t_obj, unsigned long t_index, void*, void*) const;
-		void onSetMaximized() const;
+        signals:
+                void imageReaderInitialized();
+                void imageReaderFailed(const QString& t_message);
 
-	private slots :
-		void onChangeImage(int t_index);
-		void onRenderFinished();
+        public slots:
+                void onActivateWidget(const bool& t_flag);
+                void onApplyTransformation(const transformationType& t_type) const;
+                void onRefreshScrollValues(core::Series* t_series, core::Image* t_image);
+                void onChangeScrollValue(vtkObject* t_obj, unsigned long t_index, void*, void*) const;
+                void onSetMaximized() const;
+                void onRenderFailed(const QString& t_message);
+
+        private slots :
+                void onChangeImage(int t_index);
+                void onRenderFinished();
 
 	protected:
 		void closeEvent(QCloseEvent* t_event) override;
 
 	private:
 		Ui::Widget2D m_ui = {};
-		QVTKOpenGLNativeWidget* m_qtvtkWidget = {};
-		std::unique_ptr<vtkWidgetBase> m_vtkWidget = {};
-		vtkSmartPointer<vtkEventQtSlotConnect> m_scrollConnection = {};
-		QScrollBar* m_scroll = {};
-		QFuture<void> m_future = {};
+                QVTKOpenGLNativeWidget* m_qtvtkWidget = {};
+                std::unique_ptr<vtkWidgetBase> m_vtkWidget = {};
+                vtkSmartPointer<vtkEventQtSlotConnect> m_scrollConnection = {};
+                QScrollBar* m_scroll = {};
+                QFuture<void> m_future = {};
+                QPointer<QLabel> m_errorLabel = {};
 
-		void initView() override;
-		void initData() override;
-		void createConnections() override;
-		void resetView() override;
+                void initView() override;
+                void initData() override;
+                void createConnections() override;
+                void resetView() override;
 		void connectScroll();
 		void startLoadingAnimation() override;
 		void disconnectScroll() const;
